5dff236af467fe0b957c7da98be7da57552e49b9
Rian Maulana <43876647+rianmau-lana@users.noreply.github.com>
2022-03-29 19:40:45 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <title>Login</title>
+</head>
+
+<body>
+    <section class="vh-100">
+        <div class="container py-5 h-100">
+            <div class="row d-flex align-items-center justify-content-center h-100">
+                <div class="col-md-8 col-lg-7 col-xl-6">
+                    <img src="img/login.png" class="img-fluid" alt="Phone image">
+                </div>
+                <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+                    <h1>Login Page</h1>
+                    <form method="POST">
+                        <!-- Username input -->
+                        <div class="form-outline mb-4">
+                            <label class="form-label">Username</label>
+                            <input type="text" name="username" class="form-control form-control-lg" />
+                        </div>
+
+                        <!-- Password input -->
+                        <div class="form-outline mb-4">
+                            <label class="form-label">Password</label>
+                            <input type="password" name="password" class="form-control form-control-lg" />
+                        </div>
+
+                        <!-- Submit button -->
+                        <div class="d-grid gap-2">
+
+                            <input type="submit" name="login" value="Login" class="btn btn-primary btn-lg">
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
+
+<?php
+if (isset($_POST['login'])) {
+    $username = $_REQUEST['username'];
+    $password = $_REQUEST['password'];
+
+    // function cekLogin($username, $password) {
+        if ($username == 'admin' && $password == 'mimin') {
+            header("Location: home.php");
+        }
+        echo "<script>alert('Username atau Password salah')</script>";
+    // }
+}
+?>
